81ac626739efd31f835466bb5fd388a1b093e2e8
Eric Wise <angiechangpagne@gmail.com>
2019-07-28 17:38:02 -0400

    python

---
+++ app.py
@@ -0,0 +1,55 @@
+print("Changpagne")
+
+print(' o-----')
+print(' ||||')
+
+print('*' * 10)
+
+
+price=10
+price=20
+
+print(price)
+
+social=input('What is your social')
+print('thank you'+social)
+
+favorite_city=input('what is your favorite city')
+print(social+'likes'+favorite_city)
+
+birth_year=input('Birth year: ')
+
+print(type(birth_year))
+age=2019-birth_year
+print(type(age))
+print(age)
+
+
+
+weight_lbs=input('weight lbs:')
+weight_kgs=weight_lbs*.45
+print(weight_kg)
+
+
+course='Python for beginners'
+another=course[:]
+print(another)
+print(course[-1])
+print(course[-2])
+
+
+print(course[0:3])
+print(course[:5])
+
+print(course[1:])
+
+name='Hefner'
+print(name[1:-1])
+
+first='angie'
+last='shanghai'
+message=first+'['+last+'] is a code'
+msg=f'{first}  [{last}] is a coder'
+print(message)
+
+print(msg)
