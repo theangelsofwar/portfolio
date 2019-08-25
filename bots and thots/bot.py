105de340003ea7f8dce1f38c98c0670ce09c2a03
Eric Wise <angiechangpagne@gmail.com>
2019-08-25 12:58:04 -0700

    bots and thotsssss

--- bots and thots/bot.py
+++ bots and thots/bot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
 
@@ -7,8 +8,14 @@ class InstagramBot:
     def __init__(self,username,password):
         self.username=username
         self.password=password
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        #self.driver=webdriver.Chrome('chromedriver.exe')
 
-        self.driver=webdriver.Chrome()
+        #self.driver.get('https://www.instagram.com/')
+        self.login()
+
+    def login(self):
+        self.driver.get('https://www.instagram.com/accounts/login')
 
 if __name__=='__main__':
     ig_bot=InstagramBot('temp_username','temp_password')
