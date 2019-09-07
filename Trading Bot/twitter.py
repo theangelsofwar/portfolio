4ae17bcbf842ae797bb8d137741fba934b90f48e
Eric Wise <angiechangpagne@gmail.com>
2019-09-06 19:49:29 -0700

    123

--- Trading Bot/twitter.py
+++ Trading Bot/twitter.py
@@ -211,4 +211,163 @@ class Twitter:
     def get_tweet_text(self,tweet):
         """Returns full text of a tweet"""
 
-        #forma
+        #format for getting at the full text is different depending on 
+        #whether tweet came through REST API or Streaming API:
+        #https://dev.twitter.com/overview/api/upcoming-changes-to-tweets
+        try:
+            if "extended_tweet" in tweet:
+                self.logs.debug("Decoding extended tweet from Streaming API")
+                return tweet["extended_tweet"]["full_text"]
+            elif "full_text" in tweet:
+                self.logs.debug("Decoding extended tweet from REST API")
+                return tweet["full_text"]
+            else:
+                self.logs.debug("Decoding short tweet.")
+                return tweet["text"]
+        except KeyError:
+            self.logs.error("Malformed tweet: %s" % tweet)
+            return None
+
+
+        def get_tweet_link(self,tweet):
+            """Creates link URL to a tweet"""
+
+            if not tweet:
+                self.logs.error("No tweet to get link")
+                return None
+
+            try:
+                screen_name=tweet["user"]["screen_name"]
+                id_str=tweet["id_str"]
+            except KeyError:
+                self.logs.error("Malformed tweet for link: %s" % tweet)
+                return None
+            
+
+            link=TWEET_URL % (screen_name,id_str)
+            return link
+
+
+        class TwitterListener(StreamListener):
+            """A listener class for handling streaming Twitter data"""
+
+            def __init__(self,callback,logs_to_cloud):
+                self.logs_to_cloud=logs_to_cloud
+                self.logs=Logs(name="twitter-listener",to_cloud=self.logs_to_cloud)
+                self.callback=callback
+                self.error_status=None
+                self.start_queue()
+
+
+            def start_queue(self):
+                """Creates queue and starts worke threads"""
+
+                self.queue=Queue()
+                self.stop_event=Event()
+                self.logs.debug("Starting %s worker threads." % NUM_THREADS)
+                self.workers=[]
+                for worker_id in range(NUM_THREADS):
+                    worker=Thread(target=self.process_queue,args=[worker_id])
+                    worker.daemon=True 
+                    worker.start()
+                    self.workers.append(worker)
+
+                
+            def stop_queue(self):
+                """Shuts down queue and worker threads """
+                #First stop the queue
+                if self.queue:
+                    self.logs.debug("Stopping queue.")  
+                    self.queue.join()
+                else:
+                    self.logs.warn("No queue to stop") 
+
+                #Then stop the worker threads
+                if self.workers:
+                    self.logs.debug("Stopping %d worker threads" % len(self.workers))
+                    self.stop_event.set()
+                    for worker in self.workers:
+                        # Block until thread terminates
+                        worker.join()
+                else:
+                    self.logs.warn("No worker threads to stop.")
+
+
+            def process_queue(self,worker_id):
+                """Continuously proccesses tasks on queue"""
+
+
+                #Create new logs instance (with own httplib2 instance) so there is separate one for each thread
+                logs=Logs("twitter-listener-worker-%s" % worker_id, to_cloud=self.logs_to_cloud)
+                logs.debug("Started worker thread: %s" % worker_id)
+                while not self.stop_event.is_set():
+                    try:
+                        data=self.queue.get(block=True,timeout=QUEUE_TIMEOUT_S)
+                        start_time=time()
+                        self.handle_data(logs,data)
+                        self.queue.task_done()
+                        end_time=time()
+                        qsize=self.queue.qsize()
+                        logs.debug("Worker %s took %.f ms with %d tasks remaining" %(worker_id,end_time-start_time,qsize))
+                    except Empty:
+                        logs.debug("Worker %s timed out on an empty queue" % worker_id)
+                        continue
+                    except Exception:
+                        #Main loop doesnt catch and report exceptions from background threads so do that here
+                        logs.catch()
+                logs.debug("Stopped worker thread: %s" % worker_id)
+
+
+            def on_error(self,status):
+                """Handles any API errors"""
+
+                self.logs.error("Twitter error: %s" % status)
+                self.error_status=status
+                self.stop_queue()
+                return False
+
+
+            def get_error_status(self):
+                """ Returns API error status, if there was one"""
+                return self.error_status
+
+
+            def on_data(self,data):
+                """ Puts task to process new data on queue"""
+                #Stop streaming if requested
+                if self.stop_event.is_set();
+                return False
+
+
+                #put task on queue and keep streaming
+                self.queue.put(data)
+                return True
+
+
+            def handle_data(self,logs,data):
+                """Sanity-checks and extracts the data before sending it to the callback"""
+
+                try:
+                    tweet=loads(data)
+                except ValueError:
+                    logs.error("Failed to decode JSON data: %s" % data)
+                    return
+
+
+                try: 
+                    user_id_str=tweet["user"]["id_str"]
+                    screen_name=tweet["user"]["screen_name"]
+                except KeyError:
+                    logs.error("Malformed tweet: %s" % tweet)
+                    return
+
+
+                #We're only interested in tweets from Mr. Trump himself, so skip the rest
+                if user_id_str!= TRUMP_USER_ID:
+                    logs.debug("Skipping tweet from user: %s (%s)" % (screen_name,user_id_str))
+                    return
+                
+
+                logs.info("Examining tweet: %s" % tweet)
+                # Call callback
+                self.callback(tweet)
